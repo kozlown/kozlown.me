0b0ab4967824e386152880852b59b5640be73586
Nigelk <kozlown@gmail.com>
2017-12-20 12:52:40 +0100

    About

--- src/About.js
+++ src/About.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import './About.css'
+
 const About = () => (
-  <div>
-    About
+  <div className='About'>
+    <h2>
+      Nigel Kozlowski
+    </h2>
+    <p>Computer science student at <a target='_' href="http://www.ipi-ecoles.com/">IPI</a></p>
+    <p>
+      <u>Climber</u> and <u>Web developer</u>
+    </p>
+    <p>
+      Interested by <u className='dotted'>vegan food</u> and <u className='dotted'>mental & physical development</u>
+    </p>
+    <p>
+      Javascript lover <span className="red">❤</span>
+    </p>
   </div>
 )
 export default About
--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
   Link
 } from 'react-router-dom'
 
+import './App.css'
+
 import About from './About'
 import Pictures from './Pictures'
 import WhatsHere from './WhatsHere'
@@ -14,22 +16,22 @@ import Projects from './Projects'
 
 const BasicExample = () => (
   <Router>
-    <div>
-      <ul>
-        <li><Link to="/">About</Link></li>
-        <li><Link to="/WhatsHere">WhatsHere</Link></li>
-        <li><Link to="/Projects">Projects</Link></li>
-        <li><Link to="/Recipes">Recipes</Link></li>
-        <li><Link to="/Pictures">Pictures</Link></li>
-      </ul>
+    <div className='App'>
+      <div className='nav-bar autogrid'>
+        <div className='center'><Link to="/">About</Link></div>
+        <div className='center'><Link to="/WhatsHere">What's here ?</Link></div>
+        <div className='center'><Link to="/Projects">Projects</Link></div>
+        <div className='center'><Link to="/Recipes">Recipes</Link></div>
+        <div className='center'><Link to="/Pictures">Pictures</Link></div>
+      </div>
 
-      <hr/>
-
-      <Route exact path="/" component={About}/>
-      <Route path="/WhatsHere" component={WhatsHere}/>
-      <Route path="/projects" component={Projects}/>
-      <Route path="/recipes" component={Recipes}/>
-      <Route path="/pictures" component={Pictures}/>
+      <div className='content'>
+        <Route exact path="/" component={About}/>
+        <Route path="/WhatsHere" component={WhatsHere}/>
+        <Route path="/projects" component={Projects}/>
+        <Route path="/recipes" component={Recipes}/>
+        <Route path="/pictures" component={Pictures}/>
+      </div>
     </div>
   </Router>
 )
